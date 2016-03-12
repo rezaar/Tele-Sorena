5e9ce9aed9cb0f54e7c7fa74beff40126e64044d
rezaar <reza.ar0077@gmail.com>
2016-03-12 21:30:40 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -224,7 +224,8 @@ function create_config( )
     "invite",
     "all",
     "leave_ban",
-    "admin"
+    "admin",
+    "linkpv"
     },
     sudo_users = {tonumber(62283960)},--Sudo users
     disabled_channels = {},
