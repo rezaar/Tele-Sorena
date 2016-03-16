e87af45dd04bc3ebb191a687232ebf8c8ce43446
rezaar <reza.ar0077@gmail.com>
2016-03-16 20:39:55 +0330

    Create upgradechat.lua

---
+++ plugins/upgradechat.lua
@@ -0,0 +1,1 @@
+local function run(msg, matches)  if msg.to.type == 'chat' and is_momod then chat_upgrade('chat#id'..msg.to.id, ok_cb, false) return "Chat Upgraded to SuperGroups" end end return { patterns = { "^[!/#](tosuper)$", }, run = run }
