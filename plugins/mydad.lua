74af5226447e5fb7e2fcc0b198ceef0f6f4631f7
rezaar <reza.ar0077@gmail.com>
2016-03-16 21:31:00 +0330

    Create mydad.lua

---
+++ plugins/mydad.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return "با باباییم چیکار داری ؟؟ "
+  end
+return {
+  description = "botfather", 
+  usage = "admin name: ",
+  patterns = {
+    "^[Rr][Ee][Ee][Zz][Aa][Aa][Rr],
+  },
+  run = run
+}
+end
