83b802783579591cfb38c18ae810bbc105b75f81
rezaar <reza.ar0077@gmail.com>
2016-03-20 20:08:16 +0330

    Create myuncle.lua

---
+++ plugins/myuncle.lua
@@ -0,0 +1,14 @@
+do
+
+function run(msg, matches)
+  return "عمو با شما کار دارن" 
+  end
+return {
+  description = "botfather", 
+  usage = "admin name: ",
+  patterns = {
+    "^هومان,
+  },
+  run = run
+}
+end
