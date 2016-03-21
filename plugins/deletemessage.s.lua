e27b19f9816d56fad2dccd956050b5e8f4c14e48
rezaar <reza.ar0077@gmail.com>
2016-03-21 08:33:59 +0430

    Create deletemessage.s.lua

---
+++ plugins/deletemessage.s.lua
@@ -0,0 +1,3 @@
+do  function get_message_callback(extra, success, result) if result.to.peer_type == 'channel' then if our_id == result.from.peer_id then --local del = delete_msg(result.id, ok_cb, true) else local del = delete_msg(result.id, ok_cb, true) if del == false then send_msg(extra, "deleting failed.", ok_cb, false) return end end else send_msg(extra, "", ok_cb, false) return end end local function run(msg, matches) if matches[1] == "del" and msg.reply_id then msgr = get_message(msg.reply_id,get_message_callback, get_receiver(msg)) end end return { description = "replay message and send /del to delete message", usage = {!del : delete message}, patterns = { "^[!/#](del)$" }, run = run, moderated = true, hide = true
+
+}
