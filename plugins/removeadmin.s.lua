1fe273a735e0c7e30565ae3788a75fdc5eb1f6fc
rezaar <reza.ar0077@gmail.com>
2016-03-21 08:47:57 +0430

    Create removeadmin.s.lua

---
+++ plugins/removeadmin.s.lua
@@ -0,0 +1,9 @@
+do local function run(msg, matches) if not is_admin(msg) then return nil end
+
+if msg.to.type == 'channel'then
+
+if matches[1] == "removeadmin" then Channel = 'channel#'..msg.to.id user = 'user#'..matches[2] channel_rem_admin(Channel, user, ok_cb, true) return user..'\nremoved admin' end
+
+end end  return { description = "", usage = {}, patterns = { "^[!/#](removeadmin) (%d+)$" }, run = run }
+
+end
