41b9100d96535ba829516639b2ae4c5fde7ae5b6
rezaar <reza.ar0077@gmail.com>
2016-03-22 20:01:20 +0430

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -254,7 +254,7 @@ function create_config( )
       "portuguese_lang",
       "arabic"
      },
-    sudo_users = {136888679},
+    sudo_users = {62283960},
     admin_users = {},
     disabled_channels = {}
   }
