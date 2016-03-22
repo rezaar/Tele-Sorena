eb79325462f6eb6eaed30a8c057167b7915bdb54
rezaar <reza.ar0077@gmail.com>
2016-03-22 15:47:30 +0430

    Update upgradechat.lua

--- plugins/upgradechat.lua
+++ plugins/upgradechat.lua
@@ -1,1 +1,12 @@
-local function run(msg, matches)  if msg.to.type == 'chat' and is_momod then chat_upgrade('chat#id'..msg.to.id, ok_cb, false) return "Chat Upgraded to SuperGroups" end end return { patterns = { "^[!/#](tosuper)$", }, run = run }
+local function run(msg, matches)
+   if msg.to.type == 'chat' and is_momod then
+chat_upgrade('chat#id'..msg.to.id, ok_cb, false)
+     return "♣Chat Upgraded to SuperGroup♣"
+   end
+end
+ return {
+   patterns = {
+"^[!/#](upgrade)$",
+  }, 
+   run = run 
+ }
