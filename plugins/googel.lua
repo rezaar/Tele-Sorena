95aeeae55a7166e55a030fc572bd25207290b2e2
rezaar <reza.ar0077@gmail.com>
2016-03-23 13:06:16 +0430

    Update googel.lua

--- plugins/googel.lua
+++ plugins/googel.lua
@@ -34,7 +34,7 @@ return {
   description = "Searche in Google",
   usage = "/src (item) : google search",
   patterns = {
-    "^[*#]src (.*)$",
+    "^[*!#]src (.*)$",
     "^%.[s|S]rc (.*)$"
   },
   run = run
